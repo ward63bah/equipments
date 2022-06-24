752d318fe801e2cd761f0fc9064c1b19b88a826f
sathidporn <sathidporn.c@gmail.com>
2022-06-24 13:26:35 +0700

    Update mock up data

--- src/sections/@dashboard/equipment/EquipmentDetail.js
+++ src/sections/@dashboard/equipment/EquipmentDetail.js
@@ -77,7 +77,7 @@ export default function EquipmentDetail(props) {
     });
     return images;
   }
-  const images = importAll(require.context('./../../../img/EQUIPMENT', false, /\.(png|jpe?g|svg)$/));
+  const images = importAll(require.context('./../../../img/EQUIPMENT', false, /\.(png|jpe?g|svg|jpeg|jpg)$/));
 
   const handleClickOpen = () => {
     setOpen(true);
--- src/sections/@dashboard/equipment/EquipmentImage.js
+++
@@ -1,5 +0,0 @@
-import image1 from '../../../asset/EQIP_IMAGES/CPU-60B1.jpg';
-
-export default {
-  image1,
-};
